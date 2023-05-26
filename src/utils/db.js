2b05dd876a92c8f7f70bdc9c1b2e3ff340e997bd
Adin Rama Ariyanto Putra <rp00038@gmail.com>
2023-05-26 08:08:02 +0700

    auth mongodb

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://rama:<password>@dbdeployments.bgpd7js.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://rama:V6xFVIaBcUFIogvR@dbdeployments.bgpd7js.mongodb.net/?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
