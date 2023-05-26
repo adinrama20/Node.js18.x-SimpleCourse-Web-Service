645fab1c190f19cba468acc3985e52e3fdeb6a02
Adin Rama Ariyanto Putra <rp00038@gmail.com>
2023-05-26 10:40:00 +0700

    change host

--- src/app.js
+++ src/app.js
@@ -3,15 +3,15 @@ const routes = require("./routes");
 
 const init = async () => {
   const server = Hapi.server({
-    port: 8000,
+    port: 3001,
+    host: "localhost",
     // host: "0.0.0.0",
   });
 
   server.route(routes);
 
   await server.start();
-  // console.log(`Server is running on ${server.info.uri}`);
-  console.log("Server berhasil dijalankan");
+  console.log(`Server is running on ${server.info.uri}`);
 };
 
 init();
