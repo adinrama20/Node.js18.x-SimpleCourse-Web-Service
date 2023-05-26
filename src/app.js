671afda21a6fcd748fea74eb53f6d22c837738cd
Adin Rama Ariyanto Putra <rp00038@gmail.com>
2023-05-26 08:39:48 +0700

    config deployment

--- src/app.js
+++ src/app.js
@@ -4,13 +4,14 @@ const routes = require("./routes");
 const init = async () => {
   const server = Hapi.server({
     port: 8000,
-    host: "0.0.0.0",
+    // host: "0.0.0.0",
   });
 
   server.route(routes);
 
   await server.start();
-  console.log(`Server is running on ${server.info.uri}`);
+  // console.log(`Server is running on ${server.info.uri}`);
+  console.log("Server berhasil dijalankan");
 };
 
 init();
